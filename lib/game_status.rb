bb4a725a494a8bcd93d10df9c32aaaeb2116e933
Harleigh Abel <harleighabel@gmail.com>
2018-01-25 19:51:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,9 @@ WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board,combo[0])
+    end
   end
-end
+end  
 
 def full?(board)
   board.all? {|index| index == "X" || index == "O"}
@@ -27,4 +28,4 @@ def draw(board)
   else
     false
   end
-end      
+end
