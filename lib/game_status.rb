ad27702696cc49aa77ce0140abb11e00d88c66ba
Harleigh Abel <harleighabel@gmail.com>
2018-01-25 18:07:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,12 +1,12 @@
 WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |combo| 
-    if combo == "X""X""X" || combo == "O""O""O"}
-    return combo
+  WIN_COMBINATIONS.each do |combo|
+    if combo == "X""X""X" || combo == "O""O""O"
+      return combo
     else
       false
     end
-  end  
+  end
 
 end
